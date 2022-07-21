71280caa977775efb24bafa7879068fd0f573be5
panasonikk <grebenyuk1507@gmail.com>
2022-07-21 11:46:09 +0300

    Implemented current time

--- src/app.js
+++ src/app.js
@@ -1,1 +1,24 @@
-
+function formatDate(date) {
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  let hour = date.getHours();
+  let minutes = date.getMinutes();
+  if (hour < 10) {
+    hour = `0${hour}`;
+  }
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let dateElement = document.querySelector("#date");
+  let formattedDate = `${day} ${hour}:${minutes}`;
+  dateElement.innerHTML = formattedDate;
+}
+formatDate(new Date());
