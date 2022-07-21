c4133ef4d11dc70afdb4eb8b251d0bb1e50951ce
panasonikk <grebenyuk1507@gmail.com>
2022-07-21 11:58:28 +0300

    Integrated API weather info

--- src/app.js
+++ src/app.js
@@ -21,4 +21,25 @@ function formatDate(date) {
   let formattedDate = `${day} ${hour}:${minutes}`;
   dateElement.innerHTML = formattedDate;
 }
+function handleSubmit() {
+  let city = "Paris";
+  let apiKey = "63116731662a94eebc651f7bb7447ea1";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(getWeather);
+}
+function getWeather(response) {
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+  let temperature = Math.round(response.data.main.temp);
+  let mainTemp = document.querySelector("#temperature");
+  mainTemp.innerHTML = temperature;
+
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind-speed").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].main;
+}
 formatDate(new Date());
+handleSubmit();
